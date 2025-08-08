feb607c8db88526901c6d6e7553eba8e61a71ed4
Saurabh Pandey <147328092+saurabh-1907@users.noreply.github.com>
2025-08-08 23:03:51 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { register, login, getAllUsers } = require("../controllers/authController");
+const { register, login, getAllUsers, getCurrentUser } = require("../controllers/authController");
 const { auth, adminOnly } = require("../middleware/authMiddleware");
 
 router.post("/register", register);
 router.post("/login", login);
 router.get("/users", auth, adminOnly, getAllUsers);
+router.get("/me", auth, getCurrentUser); // new route
 
 module.exports = router;
